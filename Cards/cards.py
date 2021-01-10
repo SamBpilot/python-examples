7eaa7f941590c1bb38fa75611892e6a2a7fb651e
Sam Blewitt <sam@bandlem.com>
2021-01-10 19:43:52 +0000

    Added type hints and fixed warnings
    Signed-off-by: Sam Blewitt <sam@bandlem.com>

--- Cards/cards.py
+++ Cards/cards.py
@@ -1,19 +1,25 @@
 import random
 
+
 def name(card):
-    '''Takes the initials of a card and returns its name'''
+    """Takes the initials of a card and returns its name
+    :param card: a card e.g. "AS"
+    :type card: str
+    :return: card name e.g. Ace of Spades
+    :rtype: str
+    """
     suit = card[1]
-    value = card[0]
-    if value == "A":
-        value = "Ace"
-    elif value == "K":
-        value = "King"
-    elif value == "Q":
-        value = "Queen"
-    elif value == "J":
-        value = "Jack"
-    elif value == "0":
-        value = "10"
+    val = card[0]
+    if val == "A":
+        val = "Ace"
+    elif val == "K":
+        val = "King"
+    elif val == "Q":
+        val = "Queen"
+    elif val == "J":
+        val = "Jack"
+    elif val == "0":
+        val = "10"
 
     if suit == "H":
         suit = "Hearts"
@@ -23,42 +29,59 @@ def name(card):
         suit = "Clubs"
     elif suit == "S":
         suit = "Spades"
-    return(f"{value} of {suit}")
+    return f"{val} of {suit}"
+
 
 def value(card):
-    '''Finding the value of a card'''
-    value = card[0]
-    if value == "A":
-        value = 11
-    elif value == "K":
-        value = 10
-    elif value == "Q":
-        value = 10
-    elif value == "J":
-        value = 10
-    elif value == "0":
-        value = 10
+    """Finding the value of a card
+    :param card: short hand for a card
+    :type card: str
+    :return: value
+    :rtype: int
+    """
+    val = card[0]
+    if val == "A":
+        val = 11
+    elif val == "K":
+        val = 10
+    elif val == "Q":
+        val = 10
+    elif val == "J":
+        val = 10
+    elif val == "0":
+        val = 10
     else:
-        value = int(value)
-    return value
+        val = int(val)
+    return val
 
-def handscore(cards):
+
+def hand_score(cards):
+    """Get the score of cards in a hand
+    :param cards: an array of cards
+    :type cards: list[str]
+    :return: score
+    :rtype: int
+    """
     total = 0
     for card in cards:
         card = value(card)
         total += card
-    return(total)
+    return total
+
 
 def deck():
-    '''Puts all the cards into a random order and gets an array for it'''
+    """Puts all the cards into a random order and gets an array for it
+    :return: cards
+    :rtype: list[str]
+    """
     cards = []
     for suit in ["C", "H", "S", "D"]:
-        for value in ["0", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]:
-            card = f"{value}{suit}"
+        for val in ["0", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]:
+            card = f"{val}{suit}"
             cards += [card]
 
     random.shuffle(cards)
-    return(cards)
+    return cards
 
 
 if __name__ == '__main__':
--- Cards/cards_test.py
+++ Cards/cards_test.py
@@ -4,9 +4,10 @@ import cards
 
 
 class CardsTestCase(unittest.TestCase):
-    '''All my tests'''
-    def test_cardname(self):
-        '''Tests for the function cardname'''
+    """All my tests"""
+
+    def test_card_name(self):
+        """Tests for the function card_name"""
         self.assertEqual("Ace of Spades", cards.name("AS"))
         self.assertEqual("9 of Clubs", cards.name("9C"))
         self.assertEqual("Jack of Hearts", cards.name("JH"))
@@ -18,7 +19,7 @@ class CardsTestCase(unittest.TestCase):
         self.assertEqual("Queen of Diamonds", cards.name("QD"))
 
     def test_value(self):
-        '''Tests for the function value'''
+        """Tests for the function value"""
         self.assertEqual(9, cards.value("9S"))
         self.assertEqual(10, cards.value("0S"))
         self.assertEqual(8, cards.value("8S"))
@@ -34,20 +35,19 @@ class CardsTestCase(unittest.TestCase):
         self.assertEqual(10, cards.value("KS"))
         self.assertEqual(11, cards.value("AS"))
 
-    def test_handscore(self):
-        '''Tests for the function handscore which will find the score of someone's hand'''
-        self.assertEqual(21, cards.handscore(["AS", "8C", "2D"]))
-        self.assertEqual(15, cards.handscore(["KS", "2C", "3D"]))
-        self.assertEqual(22, cards.handscore(["JS", "5C", "7D"]))
-        self.assertEqual(15, cards.handscore(["QS", "3C", "2D"]))
-        self.assertEqual(25, cards.handscore(["0S", "9C", "6D"]))
-        self.assertEqual(18, cards.handscore(["3S", "6C", "9D"]))
+    def test_hand_score(self):
+        """Tests for the function hand_score which will find the score of someone's hand"""
+        self.assertEqual(21, cards.hand_score(["AS", "8C", "2D"]))
+        self.assertEqual(15, cards.hand_score(["KS", "2C", "3D"]))
+        self.assertEqual(22, cards.hand_score(["JS", "5C", "7D"]))
+        self.assertEqual(15, cards.hand_score(["QS", "3C", "2D"]))
+        self.assertEqual(25, cards.hand_score(["0S", "9C", "6D"]))
+        self.assertEqual(18, cards.hand_score(["3S", "6C", "9D"]))
 
     def test_deck(self):
-        '''Tests for the function deck which outputs the score of a hand'''
-        self.assertEqual(380, cards.handscore(cards.deck()))
+        """Tests for the function deck which outputs the score of a hand"""
+        self.assertEqual(380, cards.hand_score(cards.deck()))
         self.assertEqual(52, len(cards.deck()))
-
 
 
 if __name__ == '__main__':
